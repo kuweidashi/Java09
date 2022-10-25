5a59eefb8777ead3ea8ad93e52b4737f69e5324b
kuweidashi <89897887+kuweidashi@users.noreply.github.com>
2022-10-25 17:18:50 +0800

    Create Class01.java

---
+++ Class01.java
@@ -0,0 +1,20 @@
+package Java09;
+public class Class01
+{
+   public static void main(String args[])
+   {
+   }
+}
+class CRectangle
+{   
+    int width;
+    int height;
+    public CRectangle(int w,int h) {
+        width=w;
+        height=h;
+    }
+    public CRectangle(){
+        this.width=10;
+        this.height=8;
+    }
+}
