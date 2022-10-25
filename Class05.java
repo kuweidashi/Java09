04d1965718d67d53c1018fb8ec1e340050fd9b63
kuweidashi <89897887+kuweidashi@users.noreply.github.com>
2022-10-25 17:31:15 +0800

    Create Class05.java

---
+++ Class05.java
@@ -0,0 +1,27 @@
+package Java09;
+public class Class05
+{
+   public static void main(String args[])
+   {
+   }
+}
+class CCount
+{   
+    int cnt=0;
+    public void count()
+    {
+        cnt += 1;
+    }
+    public CCount()
+    {
+        count();
+    }
+    public void setZero()
+    {
+        cnt = 0;
+    }
+    public void setValue(int n)
+    {
+        cnt = n;
+    }
+}
