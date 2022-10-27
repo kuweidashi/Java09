f6679966ad507e1153616403acc146907966a450
kuweidashi <89897887+kuweidashi@users.noreply.github.com>
2022-10-27 23:00:24 +0800

    Create Class06.java

---
+++ Class06.java
@@ -0,0 +1,18 @@
+package Java09;
+
+class add
+{
+   public static void add2n(int n)
+   {
+      System.out.println((1+n)*n/2.0);
+   }
+}
+
+public class Class06
+{
+   public static void main(String args[])
+   {
+      add.add2n(5);
+      add.add2n(10);
+   }
+}
